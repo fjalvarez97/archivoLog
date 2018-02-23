f0c3cf112013524f6068e17937c643c87bccc9d9
fjalvarez97 <franjalvarez955@gmail.com>
2018-02-23 13:08:19 +0100

    Creados campos y constructor de la clase acceso, y un metodo en la otra clase que lee un arhivo.log y almacena toda su informacion

--- Acceso.java
+++ Acceso.java
@@ -8,11 +8,29 @@
  */
 public class Acceso
 {
+    private int ano;
+    private int mes;
+    private int dia;
+    private int hora;
+    private int minutos;
     /**
      * Constructor for objects of class Acceso
      */
-    public Acceso()
+    public Acceso(int ano, int mes, int dia, int hora, int minutos)
     {
-
+        this.ano = ano;
+        this.mes = mes;
+        this.dia = dia;
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+    
+    /**
+     * Metodo getter del campo hora
+     * @return int hora
+     */
+    public int getHora()
+    {
+        return hora;
     }
 }
--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 /**
  * Esta clase analiza el archivo log donde se guarda la fecha en la que se han hecho 
  * accesos al servidor
@@ -11,11 +12,37 @@ import java.io.FileNotFoundException;
  */
 public class AnalizadorAccesosAServidor
 {
+    private ArrayList<Acceso> coleccionAccesos;
     /**
      * Constructor for objects of class AnalizadorAccesosAServidor
      */
     public AnalizadorAccesosAServidor()
     {
-
+        coleccionAccesos = new ArrayList<>();
     }
+
+    /**
+     * Lee y analiza un archivo.log
+     *
+     * @param  nombre Nombre del archivo que sera leido.
+     */
+    public void analizarArchivoDeLog(String nombre)
+    {
+        try {
+            File archivo = new File(nombre);
+            Scanner sc = new Scanner(archivo);
+            while (sc.hasNextLine()) {
+                String linea = sc.nextLine();
+                String[] partes = linea.split(" ");
+                Acceso nuevoAcceso = new Acceso(Integer.parseInt(partes[0]), Integer.parseInt(partes[1]),
+                Integer.parseInt(partes[2]), Integer.parseInt(partes[3]), Integer.parseInt(partes[4]));
+                coleccionAccesos.add(nuevoAcceso);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+   
 }
