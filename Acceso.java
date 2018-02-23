019cea080b53df5bdb29501ff31949129353d05d
fjalvarez97 <franjalvarez955@gmail.com>
2018-02-23 12:21:15 +0100

    Primer Commit

---
+++ Acceso.java
@@ -0,0 +1,18 @@
+
+/**
+ * Esta clase representa un unico acceso al servidor
+ *
+ * @author Fran Alvarez
+ * @version 1.0
+ * @since 2018-23-02
+ */
+public class Acceso
+{
+    /**
+     * Constructor for objects of class Acceso
+     */
+    public Acceso()
+    {
+
+    }
+}
---
+++ AnalizadorAccesosAServidor.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+/**
+ * Esta clase analiza el archivo log donde se guarda la fecha en la que se han hecho 
+ * accesos al servidor
+ *
+ * @author Fran Alvarez
+ * @version 1.0
+ * @since 2018-23-02
+ */
+public class AnalizadorAccesosAServidor
+{
+    /**
+     * Constructor for objects of class AnalizadorAccesosAServidor
+     */
+    public AnalizadorAccesosAServidor()
+    {
+
+    }
+}
